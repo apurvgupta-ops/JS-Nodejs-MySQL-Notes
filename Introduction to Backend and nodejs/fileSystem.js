c7ddad02a55e0e09191fc397e9e287056d74a621
Apurv gupta <56751664+apurvgupta-ops@users.noreply.github.com>
2024-09-04 23:35:09 +0530

    Basic of terminals

---
+++ Introduction to Backend and nodejs/fileSystem.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+
+// ? FOR CREATE THE FILE
+const file = fs.writeFileSync(
+  "C:\\Users\\Apurva\\Desktop\\test.txt",
+  "hello worldddddd"
+);
+
+// ? FOR READ THE FILE
+// const file1 = fs.readFileSync("C:\\Users\\Apurva\\Desktop\\test.txt");
+// console.log(file1.toString());
+
+// ? FOR DELETE THE FILE
+// const file2 = fs.unlinkSync("C:\\Users\\Apurva\\Desktop\\test.txt");
