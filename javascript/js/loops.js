efe61eb41a850a8ef117a007d38a5fcf2260f82c
navneetbagri <60543601+navneetbagri@users.noreply.github.com>
2024-12-30 22:08:47 +0530

    upto 48

--- javascript/js/loops.js
+++ javascript/js/loops.js
@@ -25,3 +25,62 @@ do {
   console.log(j);
   j++;
 } while (i < 5);
+
+
+// for of loop
+for(const alpha of arr){
+  console.log(alpha)
+}
+
+// for in loop
+const d = {
+  // address :@123
+  name: "Apurv",
+  age: "24",
+  ph: "123456",
+  contact: {
+    // address :@456
+    "last-name": "Gupta",
+    address: {
+      // address :@789
+      a: "Balaji tower",
+    },
+  },
+};
+
+for(const de in d){
+  console.log(d[de])
+}
+
+
+
+// MAP
+const returnValue = arr.map((item) =>{
+  return arr
+
+})
+
+
+// filter
+const filterValue = arr.filter((item) =>{
+  return item.includes("A")
+})
+
+
+// Reduce
+const reducedValue = arr.reduce((accmulator, currentValue, index) =>{
+  return accmulator + currentValue
+} ,0) // initial value
+
+
+// some => RETURN  boolean value if one get true.
+const booleanValue = arr.some((item) =>{
+  return item >3;
+})
+
+// every => RETURN boolean value if one get false.
+const booleanValues = arr.every((item) =>{
+  return item >3;
+})
+
+
