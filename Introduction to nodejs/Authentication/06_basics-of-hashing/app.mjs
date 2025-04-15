94513f995e0a23cbfa4f411ef905fc8a4f749049
Apurv Gupta <apurv.gupta@dotsquares.com>
2025-04-15 19:19:31 +0530

    hashing

--- Introduction to nodejs/Authentication/06_basics-of-hashing/app.mjs
+++ Introduction to nodejs/Authentication/06_basics-of-hashing/app.mjs
@@ -1,6 +1,8 @@
 const encodedText = new TextEncoder().encode("Hello World");
+console.log({encodedText})
 const hashBuffer = await crypto.subtle.digest("SHA-256", encodedText);
+console.log({hashBuffer})
 const hashArray = [...new Uint8Array(hashBuffer)];
+console.log({hashArray})
 const hashHex = hashArray.map(byte => byte.toString(16).padStart(2, "0")).join("");
-
 console.log(hashHex);
