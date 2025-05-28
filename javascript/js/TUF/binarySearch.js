793271189307c4f17e97e51fb2c2d8be9e3c5ba4
Apurv gupta <sahil@dotgenictechnologies.com>
2025-05-28 23:27:10 +0530

    Redis and binary serach

--- javascript/js/TUF/binarySearch.js
+++ javascript/js/TUF/binarySearch.js
@@ -311,3 +311,214 @@ Input Format: arr = [3,4,5,1,2]
 Result: 1
 Explanation: Here, the element 1 is the minimum element in the array.
 */
+
+// function fun(arr) {
+//   const n = arr.length;
+//   let low = 0;
+//   let high = n - 1;
+//   let res = arr[0];
+
+//   while (low <= high) {
+//     // If the current subarray is sorted
+//     if (arr[low] < arr[high]) {
+//       res = Math.min(res, arr[low]);
+//       break;
+//     }
+
+//     let mid = Math.floor((low + high) / 2);
+//     res = Math.min(res, arr[mid]);
+//     if (arr[low] <= arr[mid]) {
+//       low = mid + 1;
+//     } else {
+//       high = mid - 1;
+//     }
+//   }
+
+//   return res;
+// }
+
+// const arr = [3, 4, 5, 1, 2];
+// console.log(fun(arr));
+
+// ! Find out how many times the array has been rotated
+/*
+Input Format: arr = [4,5,6,7,0,1,2,3]
+Result: 4
+Explanation: The original array should be [0,1,2,3,4,5,6,7]. So, we can notice that the array has been rotated 4 times.
+
+*/
+
+// function fun(arr) {
+//   const n = arr.length;
+//   let low = 0;
+
+//   let high = n - 1;
+//   while (low <= high) {
+//     // If subarray is already sorted, return low (minimum index)
+//     if (arr[low] <= arr[high]) return low;
+
+//     let mid = Math.floor((low + high) / 2);
+//     let next = (mid + 1) % n;
+//     let prev = (mid - 1 + n) % n;
+
+//     if (arr[mid] <= arr[prev] && arr[mid] <= arr[next]) {
+//       return mid;
+//     }
+
+//     if (arr[mid])
+//       if (arr[low] <= arr[mid]) {
+//         low = mid + 1;
+//       } else {
+//         high = mid - 1;
+//       }
+//   }
+
+//   return -1;
+// }
+// const arr = [4, 5, 6, 7, 0, 1, 2, 3];
+// console.log(fun(arr));
+
+// ! Search Single Element in a sorted array
+/*
+Input Format: arr[] = {1,1,2,2,3,3,4,5,5,6,6}
+Result: 4
+Explanation: Only the number 4 appears once in the array.
+*/
+// function fun(arr) {
+//   const n = arr.length;
+//   let low = 0;
+//   let high = n - 1;
+
+//   while (low <= high) {
+//     let mid = Math.floor((low + high) / 2);
+
+//     if (mid % 2 === 1) mid--;
+
+//     if (arr[mid] === arr[mid + 1]) {
+//       low = mid + 2;
+//     }
+//     high = mid;
+//   }
+
+//   return arr[low];
+// }
+// const arr = [1, 1, 2, 2, 3, 3, 4, 5, 5, 6, 6];
+// console.log(fun(arr));
+
+// ! Peak element in Array
+/*
+Input Format: arr[] = {1,2,3,4,5,6,7,8,5,1}
+Result: 7
+Explanation: In this example, there is only 1 peak that is at index 7.
+*/
+
+// function fun(arr) {
+//   const n = arr.length;
+//   let low = 0;
+//   let high = n - 1;
+
+//   while (low <= high) {
+//     let mid = Math.floor((low + high) / 2);
+
+//     // ? edge cases
+//     // const left = mid > 0 ? arr[mid - 1] : -Infinity;
+//     // const right = mid < n - 1 ? arr[mid + 1] : -Infinity;
+
+//     if (arr[mid] >= arr[mid - 1] && arr[mid] >= arr[mid + 1]) {
+//       return mid;
+//     } else if (arr[mid] < arr[mid + 1]) {
+//       low = mid + 1;
+//     } else {
+//       high = mid - 1;
+//     }
+//   }
+//   return -1;
+// }
+
+// const arr = [1, 2, 3, 4, 5, 6, 7, 8, 5, 1];
+// console.log(fun(arr));
+
+// ! Finding Sqrt of a number using Binary Search
+
+// ? In Built Method : TC : O(logn)
+// function fun(n) {
+//   const res = Math.floor(Math.sqrt(n));
+//   return res;
+// }
+
+// ? Using Binary search
+// function fun(n) {
+//   let low = 0;
+//   let high = n;
+//   let val;
+//   while (low <= high) {
+//     let mid = Math.floor((low + high) / 2);
+//     val = mid * mid;
+//     if (val <= n) {
+//       low = mid + 1;
+//     } else {
+//       high = mid - 1;
+//     }
+//   }
+//   return high;
+// }
+
+// const n = 36;
+// console.log(fun(n));
+
+// ! Nth Root of a Number using Binary Search
+// function fun(no, n) {
+//   let low = 0;
+//   let high = no;
+
+//   while (low <= high) {
+//     let mid = Math.floor((low + high) / 2);
+//     let val = Math.pow(mid, n);
+
+//     if (val === no) {
+//       return mid;
+//     }
+
+//     if (val < no) {
+//       low = mid + 1;
+//     } else {
+//       high = mid - 1;
+//     }
+//   }
+
+//   return high;
+// }
+// const n = 3;
+// const no = 27;
+// console.log(fun(no, n));
+
+// ! Koko Eating Bananas
+/*
+Input Format: N = 4, a[] = {7, 15, 6, 3}, h = 8
+Result: 5
+Explanation: If Koko eats 5 bananas/hr, he will take 2, 3, 2, and 1 hour to eat the piles accordingly. So, he will take 8 hours to complete all the piles.  
+*/
+
+// function fun(arr, h) {
+//   let low = 1;
+//   let high = Math.max(...arr);
+//   let ans = high;
+//   while (low <= high) {
+//     let mid = Math.floor((low + high) / 2);
+//     let totalHours = 0;
+//     for (let i of arr) {
+//       totalHours += Math.ceil(i / mid);
+//     }
+//     if (totalHours <= h) {
+//       ans = mid;
+//       high = mid - 1;
+//     } else {
+//       low = mid + 1;
+//     }
+//   }
+
+//   return ans;
+// }
+// const arr = [7, 15, 6, 3];
+// const h = 8;
+// console.log(fun(arr, h));
