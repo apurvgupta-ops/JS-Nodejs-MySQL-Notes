32747ef77ca4f1926de1cc758e6d1e74e1c01497
Apurv gupta <56751664+apurvgupta-ops@users.noreply.github.com>
2026-01-02 13:54:02 +0000

    array

--- javascript/questions/array.js
+++ javascript/questions/array.js
@@ -246,8 +246,307 @@
 // Input: [1, 2, 3, 4, 5], target = 6
 // Output: (1,5), (2,4)
 
-function uniquePairs(arr, target) {
+// function uniquePairs(arr, target) {
 
-}
+// single loop(first remove the duplicate value, and then check then substract number is in the set and current number is less then the substract number , if yes then that one is the pair)
 
-console.log(uniquePairs([1, 2, 3, 4, 5], 6))
+// const set = new Set(arr)
+// const result = [];
+// for (let num of set) {
+//     const substractedValue = target - num
+//     if (set.has(substractedValue) && num < substractedValue) {
+//         result.push([num, substractedValue])
+//     }
+// }
+// return result
+
+
+// Two loops
+// const result = [];
+// for (let i = 0; i < arr.length; i++) {
+//     for (let j = i + 1; j < arr.length; j++) {
+//         if (arr[i] + arr[j] == target) {
+//             result.push([arr[i], arr[j]])
+//         }
+//     }
+// }
+// return result
+// }
+
+// console.log(uniquePairs([1, 2, 3, 4, 5], 6))
+
+
+
+// 1️⃣1️⃣ Left Shift an Array by One Position
+// Input: [1, 2, 3, 4]
+// Output: [2, 3, 4, 1] ✨ Manual shifting, moving first element to end.
+
+// function leftShiftByOne(arr) {
+//     let n = arr.length
+//     let last = arr[0]
+
+//     for (let i = 0; i < n; i++) {
+//         arr[i] = arr[i + 1]
+//     }
+
+//     arr[n - 1] = last
+
+//     return arr
+// }
+// console.log(leftShiftByOne([1, 2, 3, 4]))
+
+
+// 1️⃣2️⃣ Count How Many Times an Element Appears in an Array
+// Input: array = [1, 4, 4, 4, 2], element = 4
+// Output: 3
+
+// function freq(arr, target) {
+// with map
+// const map = new Map()
+// for (let i of arr) {
+//     map.set(i, (map.get(i) || 0) + 1)
+// }
+// return map.get(target) || 0;
+
+// without map
+// let count = 0;
+// for (let i of arr) {
+//     if (i === target) count++
+// }
+// return count;
+// }
+
+// console.log(freq([1, 4, 4, 4, 2], 4))
+
+
+
+// ! INTERMEDIATE  QUESTIONS
+
+// ? 1️⃣3️⃣ SLIDING WINDOW APPROACH **
+// We want to find the maximum of every window of size 3:
+// arr = [1, 3, -1, -3, 5, 3, 6, 7], k = 3
+//  O(n)
+
+// function maximumSum(arr, k) {
+//     let windowSum = 0
+//     for (let i = 0; i < k; i++) {
+//         windowSum += arr[i]
+//     }
+
+//     let maxSum = windowSum
+
+//     for (let j = k; j < arr.length; j++) {
+//         windowSum = windowSum - arr[j - k] + arr[j]
+//         maxSum = Math.max(maxSum, windowSum);
+//     }
+
+//     return maxSum
+// }
+// console.log(maximumSum([1, 3, -1, -3, 5, 3, 6, 7], 3))
+
+// ? 1️⃣4️⃣ KADANE'S ALGORITHM
+// arr = [ -2, 1, -3, 4, -1, 2, 1, -5, 4 ];
+// Maximum subarray sum
+
+// function maximumSubArraySum(arr) {
+//     let maxSum = 0;
+//     let currSum = 0;
+
+//     for (let i of arr) {
+//         currSum += i
+//         if (currSum > maxSum) {
+//             maxSum = currSum
+//         }
+//         if (currSum < 0) {
+//             currSum = 0
+//         }
+//     }
+//     return maxSum
+// }
+
+// console.log(maximumSubArraySum([2, 3, -7, 4, 7, -4]))
+
+// ? 1️⃣5️⃣ PREFIX SUMS ALGORITHM
+// arr = [7, 2, 5, 1, 3]
+// out = [7, 9, 14, 15, 18]
+
+// function prefixSum(arr) {
+//     let preSum = new Array(arr.length);
+//     preSum[0] = arr[0]
+
+//     for (let i = 1; i < arr.length; i++) {
+//         preSum[i] = preSum[i - 1] + arr[i]
+//     }
+
+
+//     return preSum
+// }
+
+// console.log(prefixSum([7, 2, 5, 1, 3]))
+
+
+// 1️⃣6️⃣ Find Missing Number from Range 1 to N
+// Input: [1, 5, 2, 4]
+// Output: 3
+
+// function missingNumber(arr) {
+//     let sum = 0
+//     let n = arr.length + 1
+
+//     sum = (n * (n + 1)) / 2
+
+//     for (let i of arr) {
+//         sum -= i
+//     }
+
+//     return sum
+// }
+
+// console.log(missingNumber([1, 5, 2, 4]))
+
+// 1️⃣7️⃣ Move All Zeroes to End (Stable, O(n))
+// Input: [0, 1, 0, 3, 12]
+// Output: [1, 3, 12, 0, 0]
+
+// function moveZeroes(arr) {
+
+// way 1 (space o(n))
+
+// const newArr = []
+// for (let i of arr) {
+//     if (i !== 0) {
+//         newArr.push(i)
+//     }
+// }
+// while (newArr.length < arr.length) {
+//     newArr.push(0)
+// }
+
+// return newArr
+
+// way 2 (space o(1))
+
+//     let pos = 0;
+
+//     for (let i of arr) {
+//         if (i !== 0) {
+//             arr[pos] = i;
+//             pos++;
+//         }
+//     }
+
+//     while (pos < arr.length) {
+//         arr[pos] = 0;
+//         pos++
+//     }
+
+//     return arr
+// }
+
+// console.log(moveZeroes([0, 1, 0, 3, 12]))
+
+
+// 1️⃣8️⃣  Find the First Repeating Element in an Array
+// Input: [10, 5, 3, 4, 3, 5, 6]
+// Output: 5
+
+// function firstRepeating(arr) {
+
+// way 1 o(n2)
+// for (let i = 0; i < arr.length; i++) {
+//     for (let j = i + 1; j < arr.length; j++) {
+//         if (arr[i] === arr[j]) return arr[i]
+//     }
+// }
+// return false
+
+// way 2 o(n)
+//     let seen = new Set()
+
+//     for (let i of arr) {
+//         if (seen.has(i)) return i
+//         seen.add(i)
+//     }
+
+//     return -1
+// }
+
+
+// console.log(firstRepeating([10, 5, 3, 4, 3, 5, 6]))
+
+
+// 1️⃣9️⃣ Maximum Subarray Sum (Kadane’s Algorithm – O(n))
+// Input: [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+// Output: 6
+
+// function subArraySum(arr) {
+//     let maxSum = -Infinity
+//     let sum = 0;
+//     for (let i of arr) {
+//         sum += i
+//         maxSum = Math.max(maxSum, sum)
+//         if (sum < 0) {
+//             sum = 0
+//         }
+//     }
+
+//     return maxSum
+// }
+
+// console.log(subArraySum([-2, 1, -3, 4, -1, 2, 1, -5, 4]))
+
+// 2️⃣0️⃣ Find all subarrays of an array
+// Input: [1, 2, 3]
+// Output: [[1], [1,2], [1,2,3], [2], [2,3], [3]]
+
+// function allSubarrays(arr) {
+//     let result = []
+//     for (let i = 0; i < arr.length; i++) {
+//         for (let j = i; j < arr.length; j++) {
+//             result.push(arr.slice(i, j + 1))
+//         }
+//     }
+//     return result
+// }
+// console.log(allSubarrays([1, 2, 3]))
+
+// 2️⃣1️⃣ Find all subarrays with sum = K
+// Input: [1, 2, 3, 4, 5], K = 9
+// Output: [[2,3,4], [4,5]]
+
+// function subarraysWithSumK(arr, target) {
+//     let result = []
+//     for (let i = 0; i < arr.length; i++) {
+//         let sum = 0
+//         for (let j = i; j < arr.length; j++) {
+//             sum += arr[j]
+//             if (sum === target) {
+//                 result.push(arr.slice(i, j + 1))
+//             }
+//         }
+//     }
+//     return result
+// }
+// console.log(subarraysWithSumK([1, 2, 3, 4, 5], 9))
+
+
+// 2️⃣2️⃣ Find the Longest Subarray with Sum = K (Optimized)
+// Input: [1, 2, 3, 4, 5], K = 9
+// Output: [2, 3, 4]
+
+// function LongestSubarray(arr, target) {
+//     let len = 0
+//     for (let i = 0; i < arr.length; i++) {
+//         let sum = 0;
+//         for (let j = i; j < arr.length; j++) {
+//             sum += arr[j]
+//             if (sum === target) {
+//                 len = Math.max(len, j - i + 1)
+//             }
+//         }
+//     }
+//     return len
+// }
+
+// console.log(LongestSubarray([1, 2, 3, 4, 5], 9))
+
