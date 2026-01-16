627eee97110e3a42d1344753b45a1c7ad403bd6c
Apurv gupta <sahil@dotgenictechnologies.com>
2026-01-17 00:36:24 +0530

    * static site generation

--- nextjs/app/(application)/blogs/[blogId]/page.jsx
+++ nextjs/app/(application)/blogs/[blogId]/page.jsx
@@ -2,8 +2,19 @@ import Link from "next/link";
 import { notFound } from "next/navigation";
 import React from "react";
 
+// static site generation
+export async function generateStaticParams() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos/");
+  const data = await res.json();
+
+  // always return a array of object
+  return data.map(({ id }) => ({ blogId: `${id}` }));
+}
+
 export async function generateMetadata({ params }) {
   const { blogId: id } = await params;
+
+  console.log({ id });
   return {
     title: `Blog ${id}`,
     description: `Read blog post ${id}`,
