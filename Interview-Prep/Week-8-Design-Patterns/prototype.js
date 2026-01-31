594bde9e8259f6a47ee5e20c684d670cfc353c1d
apurvgupta-ops <apurvgupta.124@gmal.com>
2026-02-01 00:52:54 +0530

    Prototype pattern

--- Interview-Prep/Week-8-Design-Patterns/prototype.js
+++ Interview-Prep/Week-8-Design-Patterns/prototype.js
@@ -4,3 +4,22 @@
 
 // Key idea:
 // Create an object → use it as a prototype → make new objects that inherit from it.
+
+const details = {
+  init(name, role) {
+    this.name = name;
+    this.role = role;
+  },
+  getInfo() {
+    return `My name is ${this.name} and his role ${this.role}`;
+  },
+};
+
+const user1 = Object.create(details);
+user1.init("Apurv", "Reactjs");
+const user2 = Object.create(details);
+user2.init("Appu", "Nodes");
+
+console.log(user1.getInfo());
+console.log(user2.getInfo());
+11;
