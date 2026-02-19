26d065a384a789001ad550875a7eb7b4f00fe9b9
apurvgupta-ops <apurvgupta.124@gmail.com>
2026-02-19 11:28:38 +0530

    change in mongo file

--- nextjs/lib/connectDb.js
+++ nextjs/lib/connectDb.js
@@ -1,17 +1,43 @@
+// import mongoose from "mongoose";
+
+// const URL = "mongodb://localhost:27017/TodoApp";
+// export const connectDB = async () => {
+//     try {
+//         if (mongoose.connection.readyState === 1) {
+//             console.log("Already Connected");
+//             return;
+//         }
+
+//         await mongoose.connect(URL);
+//         console.log("MongoDB connected successfully");
+//     } catch (error) {
+//         console.log(error);
+//         process.exit(1);
+//     }
+// };
+
 import mongoose from "mongoose";
 
+let cached = global.mongoose;
 const URL = "mongodb://localhost:27017/TodoApp";
-export const connectDB = async () => {
-    try {
-        if (mongoose.connection.readyState === 1) {
-            console.log("Already Connected");
-            return;
-        }
 
-        await mongoose.connect(URL);
-        console.log("MongoDB connected successfully");
-    } catch (error) {
-        console.log(error);
-        process.exit(1);
-    }
-};
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+export async function connectDB() {
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    cached.promise = mongoose
+      .connect(URL, {
+        family: 4, // Prevents the ::1 error
+      })
+      .then((m) => m);
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
+
+export default connectDB;
