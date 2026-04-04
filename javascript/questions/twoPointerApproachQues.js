e66790c30790d57b2b51b8616337cf29b00651d2
apurvgupta-ops <apurvgupta.124@gmail.com>
2026-04-04 12:01:45 +0530

    ques(two pointer): Move Zeros

--- javascript/questions/twoPointerApproachQues.js
+++ javascript/questions/twoPointerApproachQues.js
@@ -91,3 +91,17 @@
 // console.log(res);
 
 // Move Zeroes
+// function moveZeros(arr = [0, 1, 0, 3, 12, 4, 9]) {
+//   let left = 0;
+
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] !== 0) {
+//       [arr[left], arr[i]] = [arr[i], arr[left]];
+//       left++;
+//     }
+//   }
+//   return arr;
+// }
+
+// const res = moveZeros();
+// console.log(res);
