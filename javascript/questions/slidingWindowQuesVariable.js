ffb78410f6fabe10a8b5f5d226d1c168fb789e76
apurvgupta-ops <apurvgupta.124@gmail.com>
2026-05-04 08:59:04 +0530

    ques(sliding window variable) : sheet

--- javascript/questions/slidingWindowQuesVariable.js
+++ javascript/questions/slidingWindowQuesVariable.js
@@ -32,28 +32,78 @@ function print(fn) {
 // console.log(res);
 
 // !424. Longest Repeating Char Replace
-var characterReplacement = function (s = "AABABBA", k = 1) {
-  let map = new Map();
-  let maxFreq = 0;
-  let left = 0;
-  let maxlen = 0;
+// var characterReplacement = function (s = "AABABBA", k = 1) {
+//   let map = new Map();
+//   let maxFreq = 0;
+//   let left = 0;
+//   let maxlen = 0;
 
-  for (let right = 0; right < s.length; right++) {
-    let char = s[right];
+//   for (let right = 0; right < s.length; right++) {
+//     let char = s[right];
 
-    map.set(char, (map.get(char) || 0) + 1);
-    maxFreq = Math.max(maxFreq, map.get(char));
+//     map.set(char, (map.get(char) || 0) + 1);
+//     maxFreq = Math.max(maxFreq, map.get(char));
 
-    // let len = right - left + 1; we can't use this, becuase in while loop left get incremented, and len is outside the while loop so it check only for once(left pointer), and creates infinity loop
-    while (right - left + 1 - maxFreq > k) {
-      map.set(s[left], map.get(s[left]) - 1);
-      left++;
-    }
+//     // let len = right - left + 1; we can't use this, becuase in while loop left get incremented, and len is outside the while loop so it check only for once(left pointer), and creates infinity loop
+//     while (right - left + 1 - maxFreq > k) {
+//       map.set(s[left], map.get(s[left]) - 1);
+//       left++;
+//     }
 
-    maxlen = Math.max(maxlen, right - left + 1);
-  }
+//     maxlen = Math.max(maxlen, right - left + 1);
+//   }
 
-  return maxlen;
-};
+//   return maxlen;
+// };
 
-console.log(characterReplacement());
+// console.log(characterReplacement());
+
+// !340. Longest Substr K Distinct
+// var lengthOfLongestSubstringKDistinct = function (s = "eceba", k = 2) {
+//   let left = 0;
+//   let maxlen = 0;
+//   let map = new Map();
+
+//   for (let right = 0; right < s.length; right++) {
+//     let char = s[right];
+//     map.set(char, (map.get(char) || 0) + 1);
+
+//     while (map.size > k) {
+//       let out = s[left];
+
+//       if (map.get(out) === 1) {
+//         map.delete(out);
+//       } else {
+//         map.set(out, map.get(out) - 1);
+//       }
+
+//       left++;
+//     }
+//     maxlen = Math.max(maxlen, right - left + 1);
+//   }
+//   return maxlen;
+// };
+// console.log(lengthOfLongestSubstringKDistinct());
+
+// !713. Subarray Product Less Than K
+// var numSubarrayProductLessThanK = function (nums = [10, 5, 2, 6], k = 100) {
+//   if (k <= 1) return 0;
+//   let left = 0;
+//   let product = 1;
+//   let count = 0;
+//   for (let i = 0; i < nums.length; i++) {
+//     product *= nums[i];
+
+//     while (product >= k) {
+//       product /= nums[left];
+//       left++;
+//     }
+
+//     let len = i - left + 1;
+//     count += len;
+//   }
+
+//   return count;
+// };
+
+// console.log(numSubarrayProductLessThanK());
