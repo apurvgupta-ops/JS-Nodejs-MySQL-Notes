d7c46b98b2c78974daf94696d7632a6d2865cfee
apurvgupta-ops <apurvgupta.124@gmail.com>
2026-05-29 18:10:40 +0530

    template pattern

--- javascript/questions/FastAndSlowPointers.js
+++ javascript/questions/FastAndSlowPointers.js
@@ -237,4 +237,38 @@ class Node {
   }
 }
 
-class Solution {}
+class Solution {
+  palindromeList(head) {
+    if (!head || !head.next) {
+      return true;
+    }
+    let slow = head;
+    let fast = head;
+    while (fast !== null && fast.next !== null) {
+      slow = slow.next;
+      fast = fast.next.next;
+    }
+
+    let prev = null;
+    let current = slow;
+
+    while (current !== null) {
+      let nextNode = current.next;
+      current.next = prev;
+      prev = current;
+      current = nextNode;
+    }
+
+    let left = head;
+    let right = prev;
+
+    while (right !== null) {
+      if (left.data !== right.data) {
+        return false;
+      }
+      left = left.next;
+      right = right.next;
+    }
+    return true;
+  }
+}
