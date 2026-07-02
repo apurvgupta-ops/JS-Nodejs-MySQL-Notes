cd27016e335f14138d310d46e1a0f9884e7e82ec
apurvgupta-ops <apurvgupta.124@gmail.com>
2026-07-02 21:48:01 +0530

    CRUD spring

--- javascript/questions/Trees.java
+++ javascript/questions/Trees.java
@@ -21,17 +21,41 @@ public class Trees {
 
     // !104. Max Depth Binary Tree
     //? Standard approach to find the maximum depth of a binary tree using recursion
-    // public int maxDepth(TreeNode root) {
-    //     if (root == null) {
-    //         return 0;
-    //     }
-    //     int leftDepth = maxDepth(root.left);
-    //     int rightDepth = maxDepth(root.right);
-    //     return Math.max(leftDepth, rightDepth) + 1;
-    // }
-    //? Iterative approach to find the maximum depth of a binary tree using DFS with a stacks
-//? Iterative approach to find the maximum depth of a binary tree using BFS with a queue
-// !226. Invert Binary Tree
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+        return Math.max(leftDepth, rightDepth) + 1;
+    }
+
+    //! Iterative approach to find the maximum depth of a binary tree using DFS with a stacks
+    //! Iterative approach to find the maximum depth of a binary tree using BFS with a queue
+    // !226. Invert Binary Tree
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+
+    // !Display funtion to print the tree in a structured format
+    public void display(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        System.out.print(root.val + " ");
+        display(root.left);
+        display(root.right);
+    }
+
     public static void main(String[] args) {
         Trees tree = new Trees();
         TreeNode root = new TreeNode(1);
@@ -40,8 +64,13 @@ public class Trees {
         root.left.left = new TreeNode(4);
         root.left.right = new TreeNode(5);
 
+        System.out.println("Max Depth of the Binary Tree: "); // Output: 3
         int depth = tree.maxDepth(root);
         System.out.println("Max Depth of the Binary Tree: " + depth); // Output: 3
+
+        System.out.println("Inverted Binary Tree: ");
+        TreeNode invertedRoot = tree.invertTree(root);
+        tree.display(invertedRoot); // Output: 1 3 2 5 4
     }
 
 }
