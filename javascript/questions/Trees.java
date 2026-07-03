dcddad4546295842adc5011442b05b714c9c064c
apurvgupta-ops <apurvgupta.124@gmail.com>
2026-07-03 18:31:37 +0530

    tree

--- javascript/questions/Trees.java
+++ javascript/questions/Trees.java
@@ -64,6 +64,22 @@ public class Trees {
         return leftSum || rightSum;
     }
 
+    // !100. Same Tree
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+
+        if (p == null && q == null)
+            return true;
+
+        if (p == null || q == null)
+            return false;
+
+        if ((p.val != q.val)) {
+            return false;
+        }
+
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+
     // !Display funtion to print the tree in a structured format
     public void display(TreeNode root) {
         if (root == null) {
@@ -76,11 +92,16 @@ public class Trees {
 
     public static void main(String[] args) {
         Trees tree = new Trees();
+        // Trees tree2 = new Trees();
         TreeNode root = new TreeNode(1);
         root.left = new TreeNode(2);
         root.right = new TreeNode(3);
-        root.left.left = new TreeNode(4);
-        root.left.right = new TreeNode(5);
+        // root.left.left = new TreeNode(4);
+        // root.left.right = new TreeNode(5);
+
+        TreeNode root2 = new TreeNode(1);
+        root2.left = new TreeNode(2);
+        root2.right = new TreeNode(4);
 
         // System.out.println("Max Depth of the Binary Tree: "); // Output: 3
         // int depth = tree.maxDepth(root);
@@ -93,6 +114,9 @@ public class Trees {
         System.out.println("Path Sum Problem: ");
         System.out.println("\nPath Sum (targetSum = 22): " + tree.hasPathSum(root, 22)); // Output: false
         System.out.println("\nPath Sum (targetSum = 7): " + tree.hasPathSum(root, 7)); // Output: true
+
+        System.out.println("is Same Tree Problem: ");
+        System.out.println(tree.isSameTree(root, root2));
     }
 
 }
